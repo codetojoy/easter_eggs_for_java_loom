05463b00f65a7940c8a7b0dcbb04695c24a80177
Michael Easter <codetojoy@gmail.com>
2022-06-16 08:50:53 -0300

    clean up

--- egg_5_sc_invoke_all/set_jdk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export JAVA_HOME=/Users/measter/tools/java_19_loom_6/jdk-19.jdk/Contents/Home
-export PATH=$JAVA_HOME/bin:$PATH
-
-# source "$HOME/.sdkman/bin/sdkman-init.sh"
-# sdk env 
