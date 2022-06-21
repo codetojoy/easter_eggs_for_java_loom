025b0cf57ff05dfc1ee4f3aece84eba413eabd22
Michael Easter <codetojoy@gmail.com>
2022-06-21 16:27:27 -0300

    more doc

--- egg__10_sc_nested_scopes_thread_dump/thread-dump.sh
+++ egg__10_sc_nested_scopes_thread_dump/thread-dump.sh
@@ -11,6 +11,10 @@ fi
 
 FILE=thread.dump.json
 
+# the JEP states that the command is `JavaDump.thread`, but 
+* this thread has newer information: https://mail.openjdk.org/pipermail/loom-dev/2022-April/004139.html
+* and confirms `Thread.dump_to_file`
+
 jcmd $PID Thread.dump_to_file -format=json -overwrite $FILE 
 
 echo "wrote thread.dump.json"
