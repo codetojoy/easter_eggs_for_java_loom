a20b99f2e286537b724b52b3a70196ace7865242
Michael Easter <codetojoy@gmail.com>
2022-06-28 10:59:23 -0300

    count future states

--- egg__11b_sc_utility/src/main/java/net/codetojoy/InvokeAllScope.java
+++ egg__11b_sc_utility/src/main/java/net/codetojoy/InvokeAllScope.java
@@ -17,6 +17,9 @@ import jdk.incubator.concurrent.StructuredTaskScope;
 
 public class InvokeAllScope<T> extends StructuredTaskScope<T> {
     private final Queue<T> results = new ConcurrentLinkedQueue<>();
+    private final AtomicInteger successCounter = new AtomicInteger(0);
+    private final AtomicInteger failureCounter = new AtomicInteger(0);
+    private final AtomicInteger cancelCounter = new AtomicInteger(0);
 
     public InvokeAllScope() {
         super(null, Thread.ofVirtual().factory());
@@ -27,6 +30,20 @@ public class InvokeAllScope<T> extends StructuredTaskScope<T> {
         if (future.state() == Future.State.SUCCESS) {
             T result = future.resultNow();
             results.add(result);
+            successCounter.getAndIncrement();
+        } else if (future.state() == Future.State.FAILED) {
+            failureCounter.getAndIncrement();
+        } else if (future.state() == Future.State.CANCELLED) {
+            cancelCounter.getAndIncrement();
+        }
+    }
+
+    private void emitCounters() {
+        boolean doEmit = true;
+        if (doEmit) {
+            System.out.println("TRACER scope # success: " + successCounter.get());
+            System.out.println("TRACER scope # failure: " + failureCounter.get());
+            System.out.println("TRACER scope # cancel : " + cancelCounter.get());
         }
     }
 
@@ -36,6 +53,7 @@ public class InvokeAllScope<T> extends StructuredTaskScope<T> {
         }
 
         this.join();
+        emitCounters();
 
         return this.results.stream();
     }
