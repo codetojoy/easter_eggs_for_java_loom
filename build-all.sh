a0cc07e0583ff4773c66d7b868947e18a0f4489e
Michael Easter <codetojoy@gmail.com>
2023-07-30 10:16:43 -0300

    update build script

--- build-all.sh
+++ build-all.sh
@@ -4,50 +4,41 @@ set -e
 
 MY_HOME=$PWD
 
-cd $MY_HOME/egg_1_max_p_threads
-./clean.sh && ./compile.sh 
+clean_compile() {
+    echo "... $1"
+    cd $MY_HOME/$1
+    ./clean.sh && ./compile.sh 
+}
 
-cd $MY_HOME/egg_2_max_v_threads
-./clean.sh && ./compile.sh 
+clean_compile "egg_1_max_p_threads"
 
-cd $MY_HOME/egg_3_legacy_exec
-./clean.sh && ./compile.sh 
+clean_compile "egg_2_max_v_threads"
 
-cd $MY_HOME/egg_4_virtual_exec
-./clean.sh && ./compile.sh 
+clean_compile "egg_3_legacy_exec"
 
-cd $MY_HOME/egg_5_sc_invoke_all
-./clean.sh && ./compile.sh 
+clean_compile "egg_4_virtual_exec"
 
-cd $MY_HOME/egg_6_sc_invoke_any
-./clean.sh && ./compile.sh 
+clean_compile "egg_5_sc_invoke_all"
 
-cd $MY_HOME/egg_7_sc_deadline
-./clean.sh && ./compile.sh 
+clean_compile "egg_6_sc_invoke_any"
 
-cd $MY_HOME/egg_8_sc_custom_invoke_some
-./clean.sh && ./compile.sh 
+clean_compile "egg_7_sc_deadline"
 
-cd $MY_HOME/egg_9_sc_fan_in/client
-./clean.sh && ./compile.sh 
+clean_compile "egg_8_sc_custom_invoke_some"
 
-cd $MY_HOME/egg_9_sc_fan_in/server
-./clean.sh && ./compile.sh 
+clean_compile "egg_9_sc_fan_in/client"
 
-cd $MY_HOME/egg__10_sc_nested_scopes_thread_dump
-./clean.sh && ./compile.sh 
+clean_compile "egg_9_sc_fan_in/server"
 
-cd $MY_HOME/egg__10b_sc_nested_experiment
-./clean.sh && ./compile.sh 
+clean_compile "egg__10_sc_nested_scopes_thread_dump"
 
-cd $MY_HOME/egg__11_sc_utility
-./clean.sh && ./compile.sh 
+clean_compile "egg__10b_sc_nested_experiment"
 
-cd $MY_HOME/egg__11b_sc_utility
-./clean.sh && ./compile.sh 
+clean_compile "egg__11_sc_utility"
 
-cd $MY_HOME/egg_StackOverflow_74464598
-./clean.sh && ./compile.sh
+clean_compile "egg__11b_sc_utility"
+
+clean_compile "egg_StackOverflow_74464598"
 
 cd $MY_HOME
 
